f943e76defcb1e4118859ac975dc4b2d3dfabc71
tliang361 <78510338+tliang3612@users.noreply.github.com>
2023-02-03 17:20:09 -0800

    push

--- Assets/VolumeController.cs
+++ Assets/VolumeController.cs
@@ -11,8 +11,17 @@ public class VolumeController : MonoBehaviour
     [SerializeField] private AudioSettings _audioSettings;
     [SerializeField] private TrackType _trackType;
 
+    [SerializeField] private Image _volumeHandle;
+    [SerializeField] private Sprite _mutedSprite;
+    [SerializeField] private Sprite _unMutedSprite;
+
     private void Start()
     {
+        if (_volumeSlider.value <= 0)
+            _volumeHandle.sprite = _mutedSprite;
+        else
+
+        _volumeHandle.sprite = _unMutedSprite;
         _volumeSlider.maxValue = _audioSettings.MaxVolume;
         _text.text = ((int)(_volumeSlider.value * 20)).ToString();
         _volumeSlider.value = (float)_audioSettings.GetTrackVolume(_trackType);
@@ -22,6 +31,10 @@ public class VolumeController : MonoBehaviour
     {
         _text.text = ((int)(_volumeSlider.value * 20)).ToString();
         _audioSettings.SetTrackVolume(_trackType, _volumeSlider.value);
+        if(_volumeSlider.value <= 0)
+            _volumeHandle.sprite = _mutedSprite;
+        else
+            _volumeHandle.sprite = _unMutedSprite;
     }
 
 
